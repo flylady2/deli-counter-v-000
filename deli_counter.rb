dc06ddc7bca5815c3a7c94a798bc9cc8242a882a
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-23 01:16:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
   if katz_deli == []
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
-    katz_deli.collect.each_with_index {|name, index|
-      puts "#{index + 1}. #{name}"}.join(", ")
+    
+    new_array = katz_deli.collect.each_with_index {|name, index|
+      "#{index + 1}. #{name}"}
+    puts "The line is currently: #{new_array.join(", ")}"
 
   end
 end
